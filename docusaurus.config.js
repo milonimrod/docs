0ae62c3b56a26b29522205341253f918362b2a37
tore-statsig <74584483+tore-statsig@users.noreply.github.com>
2023-12-07 13:13:35 -0800

    Add cloud vs warehouse native selector to docs

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -50,7 +50,11 @@ module.exports = {
           type: 'html',
           position: 'right',
           value: '<button id="consoleCTA" onclick="window.open(\'https://console.statsig.com\', \'_blank\').focus(); window.statsig.logEvent({}, \'console_button_click\', window.location.pathname, {referrer: document && document.referrer,});">Get Started</button>',
-        }
+        },
+        {
+          type: 'custom-warehouseToggle', 
+          position: "left",
+        },
       ],
     },
     footer: {
